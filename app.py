042c3c9cce6092aea45198c27faef54d7ae28547
Liam Isaacs <yeqiu194@gmail.com>
2021-04-12 15:15:02 -0700

    add darkmode for no reason

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from flask import *
+
 import shap
 from shap.plots._force_matplotlib import draw_additive_plot
 
-from .models import give_shap_plot
+from model import give_shap_plot
 
 
 app = Flask(__name__)
@@ -13,7 +15,6 @@ def displayshap():
     explainer, shap_values = give_shap_plot()
 
     def _force_plot_html(explainer, shap_values, ind):
-        ind = list(ind)[0]
         force_plot = shap.plots.force(shap_values[ind], matplotlib=False)
         shap_html = f"<head>{shap.getjs()}</head><body>{force_plot.html()}</body>"
         return shap_html
@@ -21,5 +22,10 @@ def displayshap():
     shap_plots = {}
 
     for i in range(10):
+        ind = i
         shap_plots[i] = _force_plot_html(explainer, shap_values, ind)
-    return render_template('templates/displayshap.html', shap_plots = shap_plots)
+    return render_template('displayshap.html', shap_plots = shap_plots)
+
+
+if __name__ == '__main__':
+    app.run()
--- model.py
+++ model.py
@@ -2,15 +2,14 @@
 #dataset and methodology borrowed form the SHAP documentation, found here:
 # https://shap.readthedocs.io/en/latest/example_notebooks/tabular_examples/linear_models/Sentiment%20Analysis%20with%20Logistic%20Regression.html#Fit-a-linear-logistic-regression-model
 
-
 import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 import numpy as np
 import shap
 
-def give_shap_plot(): 
-    
+def give_shap_plot():
+
   corpus,y = shap.datasets.imdb()
   corpus_train, corpus_test, y_train, y_test = train_test_split(corpus, y, test_size=0.2, random_state=7)
 
@@ -23,5 +22,5 @@ def give_shap_plot():
 
   explainer = shap.Explainer(model, X_train, feature_names=vectorizer.get_feature_names())
   shap_values = explainer(X_test)
-  
+
   return explainer, shap_values
